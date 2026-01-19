7453b8da42fa44a90489ae299922aae1508c5ac2
daniel-dev-ubuntu <danielbritto989@gmail.com>
2026-01-18 23:54:30 -0300

    Add files via upload

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Adm
- */
 
 import java.sql.PreparedStatement;
 import java.sql.Connection;
@@ -23,16 +15,85 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
+    
+        conn = new conectaDAO().connectDB();       
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
         
-        
-        //conn = new conectaDAO().connectDB();
+        try {
+            prep = conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            
+            prep.executeUpdate();
+                      
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar: " + e.getMessage());
+        } finally {
+            try {
+                if (prep != null) prep.close();
+                if (conn != null) conn.close();
+                
+            } catch (Exception e) {
+               System.out.println("Erro ao fechar conexão: " + e.getMessage());
+            }
+            
+        } 
         
         
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
+        listagem.clear();
+        String sql = "SELECT * FROM produtos";
+        conn = new conectaDAO().connectDB();
+        
+        try {
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+            
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                
+                listagem.add(produto);
+                
+                
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar: " + e.getMessage());
+        } finally {
+            try {
+                if (resultset != null) resultset.close();
+                if (prep != null) prep.close();
+                if (conn != null) conn.close();
+                
+            } catch (Exception e) {
+               System.out.println("Erro ao fechar conexão: " + e.getMessage());
+            }
+            
+        } 
         
         return listagem;
+    }
+    
+    public void venderProduto(int id) {
+        String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
+        conn = new conectaDAO().connectDB();
+        
+        try {
+            prep = conn.prepareStatement(sql);
+            prep.setInt(1, id);
+            
+            prep.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Status do produto atualizado com sucesso!");
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Erro ao vender produto: " + e.getMessage());
+        }
     }
     
     
---
+++ src/UC11.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS uc11;
+USE uc11;
+
+CREATE TABLE `produtos` (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nome` text DEFAULT NULL,
+  `valor` int(11) DEFAULT NULL,
+  `status` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+INSERT INTO `produtos` (`id`, `nome`, `valor`, `status`) VALUES
+(2, 'PS4', 1500, 'Vendido'),
+(3, 'Xbox 360', 800, 'Vendido'),
+(4, 'Iphone 12', 4800, 'Vendido'),
+(5, 'PS2', 400, 'A Venda');
